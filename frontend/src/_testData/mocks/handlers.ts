26587022c3bf51a607d9d927c1ad3260f0db92fe
Vyacheslav Krasnyanskiy <VVKrasnyanskiy@sberbank.ru>
2021-03-06 23:17:28 +0300

    added mocks

---
+++ frontend/src/_testData/mocks/handlers.ts
@@ -0,0 +1,12 @@
+import { rest } from 'msw';
+import app from './data/app.json';
+
+
+const handlers = [
+    rest.get(`/app`, (req, res, ctx) =>
+        res(ctx.json(app), ctx.set('Content-Type', 'application/json'), ctx.delay(500), ctx.status(200))
+    ),
+
+];
+
+export default handlers;
