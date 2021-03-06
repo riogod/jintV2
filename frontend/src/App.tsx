3006e48b01ea0630fd84bf621087cfd8c23de40c
Краснянский Вячеслав Вячеславович <krasnyanskiy.v.v@sberbank.ru>
2021-03-07 01:10:54 +0300

    added i18next for localization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
 
-    const handleSubmit = () => {
-            fetch('/app', {
-                method: 'GET'
-            })
-                .then((res) => res.json())
-                // Update the state with the received response
-                .then((res) => {
-                    console.log(res)
-                })
-        }
+  const handleSubmit = () => {
+    fetch('/app', {
+      method: 'GET'
+    })
+      .then((res) => res.json())
+      // Update the state with the received response
+      .then((res) => {
+        console.log(res)
+      })
+  }
+
+  const { t } = useTranslation();
 
   return (
     <div className="App">
       <header className="App-header">
+        {t('title')}
         <button
           onClick={handleSubmit}
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './bootstrap/config/i18n';
 
 
-    if (process.env.NODE_ENV === 'development') {
-        // eslint-disable-next-line @typescript-eslint/no-require-imports
-        const { worker } = require('./_testData/mocks/browser');
-         worker.start()
-    }
+if (process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { worker } = require('./_testData/mocks/browser');
+    worker.start()
+}
 
-    ReactDOM.render(<App />, document.getElementById('root'))
+const Loader = () => (
+    <div className="App">
+        <div>loading...</div>
+    </div>
+);
+
+ReactDOM.render(
+    <Suspense fallback={<Loader />}>
+        <App />
+    </Suspense>,
+    document.getElementById('root')
+);
 
